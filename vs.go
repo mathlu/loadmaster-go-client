ec03992acf713befb6f52808116e00a4fdc1841e
Mathias Lundgren <mathias.lundgren@gmail.com>
2023-03-16 19:12:33 +0000

    Add function for creating virtual service

--- vs.go
+++ vs.go
@@ -20,9 +20,8 @@ type Vs struct {
 }
 
 type ApiPayLoad struct {
-	ApiKey  string `json:"apikey"`
-	CMD     string `json:"cmd"`
-	VsIndex int    `json:"vs"`
+	ApiKey string `json:"apikey"`
+	CMD    string `json:"cmd"`
 }
 
 type VsApiPayLoad struct {
@@ -63,10 +62,10 @@ func (c *Client) GetAllVs() ([]Vs, error) {
 }
 
 func (c *Client) GetVs(index int) (*Vs, error) {
-	payload := ApiPayLoad{
-		ApiKey:  c.ApiKey,
-		CMD:     "showvs",
-		VsIndex: index,
+	payload := VsApiPayLoad{
+		Vs{Index: index},
+		ApiPayLoad{ApiKey: c.ApiKey,
+			CMD: "showvs"},
 	}
 	b, err := json.Marshal(payload)
 	if err != nil {
@@ -92,7 +91,34 @@ func (c *Client) GetVs(index int) (*Vs, error) {
 	return &vs, nil
 }
 
-func (c *Client) CreateVs() (*Vs, error) {
+func (c *Client) CreateVs(ip string, proto string, port string) (*Vs, error) {
+	payload := VsApiPayLoad{
+		Vs{Address: ip,
+			Protocol: proto,
+			Port:     port},
+		ApiPayLoad{ApiKey: c.ApiKey,
+			CMD: "addvs"},
+	}
+	b, err := json.Marshal(payload)
+	if err != nil {
+		return nil, err
+	}
+
+	req, err := http.NewRequest("POST", fmt.Sprintf("%s/accessv2", c.RestUrl), bytes.NewBuffer(b))
+	if err != nil {
+		return nil, err
+	}
+
+	resp, err := c.doRequest(req)
+	if err != nil {
+		return nil, err
+	}
+
 	var vs Vs
+	err = json.Unmarshal(resp, &vs)
+	if err != nil {
+		return nil, err
+	}
+
 	return &vs, nil
 }
--- vs_test.go
+++ vs_test.go
@@ -58,6 +58,32 @@ func TestGetVs(t *testing.T) {
 
 }
 
+func TestCreateVs(t *testing.T) {
+	content, err := ioutil.ReadFile("test_data/addvs.json")
+	ok(t, err)
+	// Start a local HTTP server
+	server := httptest.NewServer(http.HandlerFunc(func(rw http.ResponseWriter, req *http.Request) {
+		// Test request parameters
+		equals(t, req.URL.String(), "/accessv2")
+		// Send response to be tested
+		_, err := rw.Write([]byte(content))
+		if err != nil {
+			fmt.Printf("Write failed: %v", err)
+		}
+	}))
+
+	defer server.Close()
+	client := Client{server.Client(), "bar", server.URL}
+
+	vs, err := client.CreateVs("192.168.1.235", "tcp", "6443")
+	ok(t, err)
+
+	equals(t, vs.Address, "192.168.1.235")
+	equals(t, vs.Protocol, "tcp")
+	equals(t, vs.Port, "6443")
+
+}
+
 func TestMarshalJSON(t *testing.T) {
 	var vs VsApiPayLoad
 	vs.Address = "192.168.1.10"
